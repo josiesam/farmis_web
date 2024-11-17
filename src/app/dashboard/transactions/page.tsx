f510701b60a74772069d47f52c702cc6097ccf40
josiesam <josiesam369@gmail.com>
2024-11-17 14:10:39 -0800

    fix linting errors 2

---
+++ src/app/dashboard/transactions/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Transaction = (props: Props) => {
+  return (
+    <div style={{ textAlign: "center", fontSize: "2em", fontWeight: 700 }}>
+      Event
+    </div>
+  )
+}
+
+export default Transaction
